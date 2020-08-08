afdbf7594660e4970c9557a3d876de06b2a93379
Lucas Vasconcelos <lucas.vasconcelos915@gmail.com>
2020-08-08 11:36:43 -0300

    Configuração do banco de dados.

--- src/server.js
+++ src/server.js
@@ -23,23 +23,76 @@ const proffys = [
 }
 ]
 
+const subjects = [
+
+    "Artes",
+    "Biologia",
+    "Ciências",
+    "Educação",
+    "Física",
+    "Geografia",
+    "História",
+    "Matemática",
+    "Português",
+    "Química",
+]
+
+const weekdays = [
+    "Domingo",
+    "Segunda-feira",
+    "Terça-feira",
+    "Quarta-feira",
+    "Quinta-feira",
+    "Sexta-feira",
+    "Sábado",
+]
+
+function getSubject(subjectNumber) {
+    return subjects[+subjectNumber - 1]
+}
+
+function pageLanding(req, res) {
+    return res.render("index.html")
+}
+
+function pageStudy(req, res) {
+    const filters = req.query
+    return res.render("study.html", { proffys, filters, subjects, weekdays })
+}
+
+function pageGiveClasses(req, res) {
+    const data = req.query
+    
+    const isNotEmpty = Object.keys(data).length  > 0
+
+    //adicionar ao obj proffys
+    if (isNotEmpty) {
+
+        data.subject = getSubject(data.subject)
+
+        proffys.push(data)
+
+        return res.redirect("/study")
+    }
+    else 
+        return res.render("give-classes.html", { subjects, weekdays })
+
+}
+
 const express = require('express')
 const server = express()
-const nunjucks = require('nunjucks')
 
+// configura nunjucks
+const nunjucks = require('nunjucks')
 nunjucks.configure('src/views', {
     express: server,
     noCache: true,
 })
 
-server.use(express.static("public"))
-.get("/", (req, res) => {
-    return res.render("index.html") 
-})
-.get("/study", (req,res) => {
-    return res.render("study.html", { proffys }) 
-})
-.get("/give-classes", (req,res) => {
-    return res.render("give-classes.html") 
-})
+server
+.use(express.static("public")) //Configura os arquivos estátivos (css, scrips, imagens)
+//rotas da aplicação
+.get("/", pageLanding)
+.get("/study", pageStudy)
+.get("/give-classes", pageGiveClasses)
 .listen(5500)
