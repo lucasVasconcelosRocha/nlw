a3f5fad35a491847f973d84b8a095b0f5e8d2463
Lucas Vasconcelos <lucas.vasconcelos915@gmail.com>
2020-08-08 07:41:16 -0300

    Configurações iniciais do node

---
+++ src/server.js
@@ -0,0 +1,45 @@
+const proffys = [
+   { 
+       name: "Diogo Fernandes", 
+       avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;             u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4", 
+       whatsapp: "99837483", 
+       bio: "Entusiasta das melhores tecnologias de química avançada. Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000  pessoas já passaram por uma das minhas explosões.", 
+       subject: "Química", 
+       cost: "20", 
+       weekday: [0], 
+       time_from: [720], 
+       time_to: [1220]
+   },
+   { 
+    name: "Diogo Fernandes", 
+    avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;             u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4", 
+    whatsapp: "99837483", 
+    bio: "Apaixonado por explodir    coisas em laboratório e por mudar a vida das pessoas através de      experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.", 
+    subject: "Química", 
+    cost: "20", 
+    weekday: [0], 
+    time_from: [720], 
+    time_to: [1220]
+}
+]
+
+const express = require('express')
+const server = express()
+const nunjucks = require('nunjucks')
+
+nunjucks.configure('src/views', {
+    express: server,
+    noCache: true,
+})
+
+server.use(express.static("public"))
+.get("/", (req, res) => {
+    return res.render("index.html") 
+})
+.get("/study", (req,res) => {
+    return res.render("study.html", { proffys }) 
+})
+.get("/give-classes", (req,res) => {
+    return res.render("give-classes.html") 
+})
+.listen(5500)
