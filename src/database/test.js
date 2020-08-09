cc7c9b8686f037e3b8d19ccbd5887f1df0df1cad
Lucas Vasconcelos <lucas.vasconcelos915@gmail.com>
2020-08-09 20:41:24 -0300

    Finalização do Next  Level Week

--- src/database/test.js
+++ src/database/test.js
@@ -1,10 +1,10 @@
 const Database = require('./db')
 const createProffy = require('./createProffy')
 
-Database.then((db) => {
+Database.then(async (db) => {
     
     proffyValue = {
-        name: "Diogo Fernandes", 
+       name: "Diogo Fernandes", 
        avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4", 
        whatsapp: "99837483", 
        bio: "Entusiasta das melhores tecnologias de química avançada. Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000  pessoas já passaram por uma das minhas explosões.", 
@@ -16,7 +16,7 @@ Database.then((db) => {
 
     }
 
-    classScheduleValue = [
+    classScheduleValues = [
        {
             weekday: 1,
             time_from: 720,
@@ -28,5 +28,34 @@ Database.then((db) => {
             time_to: 1300
         }
     ]
+
+    //await createProffy(db, {proffyValue, classValue, classScheduleValues})
+
+    const selectedProffys = await db.all("SELECT * FROM proffys")
+
+    const selectClassesAndProffys = await db.all(`
+        SELECT 
+           classes.*, 
+           proffys.*
+        FROM
+           proffys JOIN ON (
+               classes.proffy_id = proffys.id
+           )
+        WHERE
+            classes.proffy_id = 1
+    `)
+
+    const selectClassesSchedules = await db.all(`
+        SELECT 
+            class_schedule.*
+        FROM
+            class_schedule
+        WHERE
+            class_schedule.class_id = 1
+        AND class_schedule.weekday = "0"    
+        AND class_schedule.time_from <= "1200"
+        AND class_schedule.time_to > "1250"
+    
+    `)
 
 })
--- src/server.js
+++ src/server.js
@@ -1,86 +1,8 @@
-const proffys = [
-   { 
-       name: "Diogo Fernandes", 
-       avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;             u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4", 
-       whatsapp: "99837483", 
-       bio: "Entusiasta das melhores tecnologias de química avançada. Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000  pessoas já passaram por uma das minhas explosões.", 
-       subject: "Química", 
-       cost: "20", 
-       weekday: [0], 
-       time_from: [720], 
-       time_to: [1220]
-   },
-   { 
-    name: "Diogo Fernandes", 
-    avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;             u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4", 
-    whatsapp: "99837483", 
-    bio: "Apaixonado por explodir    coisas em laboratório e por mudar a vida das pessoas através de      experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.", 
-    subject: "Química", 
-    cost: "20", 
-    weekday: [0], 
-    time_from: [720], 
-    time_to: [1220]
-}
-]
-
-const subjects = [
-
-    "Artes",
-    "Biologia",
-    "Ciências",
-    "Educação",
-    "Física",
-    "Geografia",
-    "História",
-    "Matemática",
-    "Português",
-    "Química",
-]
-
-const weekdays = [
-    "Domingo",
-    "Segunda-feira",
-    "Terça-feira",
-    "Quarta-feira",
-    "Quinta-feira",
-    "Sexta-feira",
-    "Sábado",
-]
-
-function getSubject(subjectNumber) {
-    return subjects[+subjectNumber - 1]
-}
-
-function pageLanding(req, res) {
-    return res.render("index.html")
-}
-
-function pageStudy(req, res) {
-    const filters = req.query
-    return res.render("study.html", { proffys, filters, subjects, weekdays })
-}
-
-function pageGiveClasses(req, res) {
-    const data = req.query
-    
-    const isNotEmpty = Object.keys(data).length  > 0
-
-    //adicionar ao obj proffys
-    if (isNotEmpty) {
-
-        data.subject = getSubject(data.subject)
-
-        proffys.push(data)
-
-        return res.redirect("/study")
-    }
-    else 
-        return res.render("give-classes.html", { subjects, weekdays })
-
-}
 
 const express = require('express')
 const server = express()
+
+const { pageLanding, pageStudy, pageGiveClasses, saveClasses } = require('./pages')
 
 // configura nunjucks
 const nunjucks = require('nunjucks')
@@ -90,9 +12,12 @@ nunjucks.configure('src/views', {
 })
 
 server
+// receber os dados do req body
+.use(express.urlencoded({ extended: true }))
 .use(express.static("public")) //Configura os arquivos estátivos (css, scrips, imagens)
 //rotas da aplicação
 .get("/", pageLanding)
 .get("/study", pageStudy)
 .get("/give-classes", pageGiveClasses)
+.post("/save-classes", saveClasses)
 .listen(5500)
